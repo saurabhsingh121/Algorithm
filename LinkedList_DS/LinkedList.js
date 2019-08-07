bfdf6aa20d97ce2ae0daa3bc293214314b6a58a5
saurabhsingh121 <saurabhsingh121@gmail.com>
2019-08-08 01:19:36 +0530

    implement LinkedList DS in javascript

---
+++ LinkedList_DS/LinkedList.js
@@ -0,0 +1,89 @@
+class Node {
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+}
+
+class LinkedList {
+     constructor(value) {
+         this.head = {
+             value: value,
+             next: null
+         },
+         this.tail = this.head;
+         this.length = 1;
+     }
+
+     append(value) {
+        const newNode = new Node(value)
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+
+        return this;
+     }
+
+     prepend(value) {
+         const newNode = new Node(value);
+         newNode.next = this.head;
+         this.head = newNode;
+         this.length++;
+         return this;
+     }
+
+     printList() {
+         const array = [];
+         let currentNode = this.head;
+         while(currentNode !== null){
+             array.push(currentNode.value);
+             currentNode = currentNode.next;
+         }
+         return array;
+     }
+
+     insert(index, value){
+         //check params
+         if(index >= this.length) {
+            return this.append(value);
+         }
+         const newNode = new Node(value);
+         const leader = this.traverseToIndex(index-1);
+         const holdingPointer = leader.next;
+         leader.next = newNode;
+         newNode.next = holdingPointer;
+         this.length++;
+         return this;
+     }
+
+     traverseToIndex(index){
+         let counter = 0;
+         let currentNode = this.head;
+         while(counter !== index){
+             currentNode = currentNode.next;
+             counter++;
+         }
+
+         return currentNode;
+     }
+
+     remove(index){
+         const leader = this.traverseToIndex(index-1);
+         const unwantedNode = leader.next;
+         leader.next = removingPointer.next;
+         this.length--;
+         return this;
+     }
+}
+
+const myLinkedList = new LinkedList(5);
+myLinkedList.append(10);
+myLinkedList.append(20);
+myLinkedList.prepend(30);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(3);
+console.log('====================================');
+console.log(myLinkedList.printList());
+console.log('====================================');
