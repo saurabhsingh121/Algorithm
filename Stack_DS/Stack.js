3c948a47fc3a8e1668cf5df897b89ddb71bf03ba
saurabhsingh121 <saurabhsingh121@gmail.com>
2019-08-10 01:29:17 +0530

    add comment for clarification

--- Stack_DS/Stack.js
+++ Stack_DS/Stack.js
@@ -13,17 +13,22 @@ class Stack {
     }
 
     peek() {
+        //getting the first element of the stack
         return this.top;
     }
 
     push(value){
         const newNode = new Node(value);
+        //if this is first entry
         if(this.length === 0){
             this.top = newNode;
             this.bottom = newNode;
         } else {
+            //getting the interim records in holder pointer
             const holdingpointer = this.top;
+            //assigning new node to the top node
             this.top = newNode;
+            //assigning the interim node to the next of the top node
             this.top.next = holdingpointer;
         }
         this.length++;
@@ -31,12 +36,16 @@ class Stack {
     }
 
     pop(){
+        //returning null if there is no element
         if(!this.top){
             return null;
         }
+        //if there is only one record, then setting bottom node as null
         if(this.top === this.bottom){
             this.bottom = null;
         }
+        //assinging the following node of top to the top itself,
+        //and thus removing the interim top node
         this.top = this.top.next;
         this.length--;
         return this;
