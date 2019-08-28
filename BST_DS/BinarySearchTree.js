ab565abd66d201ee27a5e78f1d5400cfb446ad53
saurabhsingh121 <saurabhsingh121@gmail.com>
2019-08-28 23:37:28 +0530

    implement BST DS in javascript

---
+++ BST_DS/BinarySearchTree.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){}
+    lookup(value){}
+}
+
+const tree = new BinarySearchTree();
