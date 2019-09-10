767c971c14d648d3ddb7f5412190a7a6c38cb073
saurabhsingh121 <saurabhsingh121@gmail.com>
2019-09-11 00:01:51 +0530

    add fibonacci problem

---
+++ Recursion_Algo/FibonacciRecursive.js
@@ -0,0 +1,18 @@
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
+
+console.log(fibonacciRecursive(8));
+console.log(fibonacciIterative(8));
